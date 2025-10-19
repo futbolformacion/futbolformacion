e18cb3daafe06b9afaf46a8a982176a6b21ecb2a
futbolformacion <ivanchavesbermejo@gmail.com>
2025-10-19 16:19:51 +0200

    vite build working locally

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Configuración para despliegue en Vercel
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',      // 📁 Carpeta de salida del build
-    sourcemap: false,    // Opcional: menos peso en el deployment
-  },
-  server: {
-    port: 3000,          // Para entorno local
-  }
+  build: { outDir: 'dist' }
 })
